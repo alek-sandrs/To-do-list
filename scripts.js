c69a3627618139358b73984c4e79f85722f1e4d4
alek-sandr <loceksasa@gmail.com>
2021-04-27 16:16:46 +0300

    labojums

--- scripts.js
+++ scripts.js
@@ -1,3 +1,37 @@
-document.getElementById('darbalapa').addEventListener('click', () => {
-    console.log("Poga nospiesta!")
+const POP_UP = document.getElementById('popUp');
+let darbi = []
+
+document.getElementById('JaunaisUzdevums').addEventListener('click', () => {
+    POP_UP.style.display = 'block'
+
 })
+
+document.getElementById('PievienotUzdevumu').addEventListener('click', () => {
+    POP_UP.style.display = 'none'
+
+    let uzdevumi = {uzdevums: uzdevums.value, termins: termins.value};
+
+    uzdevums.value = "";
+    termins.value = "";
+
+    darbi.push(uzdevumi);
+
+    render();
+})
+
+
+function render() {
+    let biblioteka = document.getElementById('biblioteka');
+    biblioteka.innerHTML = ""
+
+    for(let i = 0; i < darbi.length; i++){
+        let uzdevumi = ` 
+        <div class='uzdevumi'>
+            <h3>Uzdevums: ${darbi[i].uzdevums}</h4>
+            <h4>Termiņš: ${darbi[i].termins}</h4>
+        </div>`
+        biblioteka.innerHTML += uzdevumi;
+    }
+
+
+}
